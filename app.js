fc212a3c62fff877d4fd64e4628a0acdcdea0bd8
2himitu <hmhnes@gmail.com>
2022-05-26 18:16:23 +0900

    수정 API구현

--- app.js
+++ app.js
@@ -50,4 +50,53 @@ app.get(`/wise-sayings/random`,async(req,res)=>{
   });
 });
 
+app.patch(`/wise-sayings/:id`,async(req,res)=>{
+  const {id}=req.params;
+  const [[wiseSayingsRows]] = await pool.query(
+    `
+    SELECT *
+    FROM wise_saying
+    WHERE id = ?
+    `,
+    [id]
+  ); 
+  if (wiseSayingsRows === undefined){
+    res.status(404).json({
+      resultCode : "F-1",
+      msg:"404 not found ",
+    });
+    return res;
+  }
+  const {
+    content  =wiseSayingsRows.content,
+    author  =wiseSayingsRows.author,
+    goodLikeCount  =wiseSayingsRows.goodLikeCount,
+    badLikeCount =wiseSayingsRows.badLikeCount 
+  }=req.body;
+  await pool.query(
+    `
+    UPDATE wise_saying
+    SET 
+    content  =?,
+    author  =?,
+    goodLikeCount  =?,
+    badLikeCount =?
+    WHERE id =?
+    `,
+    [content,author,goodLikeCount,badLikeCount,id]
+  );
+  const [[justModifiedWiseSayingsRows]] = await pool.query(
+    `
+    SELECT *
+    FROM wise_saying
+    WHERE id = ?
+    `,
+    [id]
+  ); 
+  res.json({
+    resultCode:"S-1",
+    msg:`${id}번이 수정되었습니다.`,
+    data : justModifiedWiseSayingsRows,
+  });
+});
 app.listen(post);
